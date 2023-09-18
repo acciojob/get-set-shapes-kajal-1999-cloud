44a7bc10d6063ac5ada114d05201673e4109f3d4
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-09-18 13:50:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+
+  getPerimeter() {
+    return 4 * this.width;
+  }
+}
+
+// Example usage:
+
+const rectangle = new Rectangle(5, 8);
+console.log('Rectangle Width:', rectangle.width); // Output: 5
+console.log('Rectangle Height:', rectangle.height); // Output: 8
+console.log('Rectangle Area:', rectangle.getArea()); // Output: 40
+
+const square = new Square(6);
+console.log('Square Side:', square.width); // Output: 6
+console.log('Square Area:', square.getArea()); // Output: 36
+console.log('Square Perimeter:', square.getPerimeter()); // Output: 24
+
 window.Rectangle = Rectangle;
 window.Square = Square;
